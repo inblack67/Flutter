0ce3b95be53e9c6fef66d16f8ad3c071ecd1252a
ryuzaki <ryuzaki>
2021-06-22 20:56:39 +0530

    network image

--- basics/lib/main.dart
+++ basics/lib/main.dart
@@ -15,15 +15,20 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.red[500],
       ),
       body: Center(
-          child: Text(
-        'Hello',
-        style: TextStyle(
-          color: Colors.red[500],
-          fontWeight: FontWeight.bold,
-          fontSize: 20.0,
-          fontFamily: 'Montserrat',
-        ),
+          child: Image(
+        image: NetworkImage(
+            'https://images.unsplash.com/photo-1517999144091-3d9dca6d1e43?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=282&q=80'),
       )),
+      // body: Center(
+      //     child: Text(
+      //   'Hello',
+      //   style: TextStyle(
+      //     color: Colors.red[500],
+      //     fontWeight: FontWeight.bold,
+      //     fontSize: 20.0,
+      //     fontFamily: 'Montserrat',
+      //   ),
+      // )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('Click'),
