739432b97bdc026e59ea0945f571819c5d33b2a6
ryuzaki <ryuzaki>
2021-06-22 21:28:35 +0530

    icon btns

--- basics/lib/main.dart
+++ basics/lib/main.dart
@@ -14,31 +14,17 @@ class MyApp extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[500],
       ),
-      body: Center(
-          child: Image(image: AssetImage('assets/images/black.jpg')
-              /*
-        NetworkImage(
-            'https://images.unsplash.com/photo-1517999144091-3d9dca6d1e43?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=282&q=80'),
-         */
-              )),
-      //     child: Image(
-      //   image: NetworkImage(
-      //       'https://images.unsplash.com/photo-1517999144091-3d9dca6d1e43?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=282&q=80'),
-      // )),
-      // body: Center(
-      //     child: Text(
-      //   'Hello',
-      //   style: TextStyle(
-      //     color: Colors.red[500],
-      //     fontWeight: FontWeight.bold,
-      //     fontSize: 20.0,
-      //     fontFamily: 'Montserrat',
-      //   ),
-      // )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('Click'),
-        backgroundColor: Colors.red[500],
+      body: Container(
+        child: Center(
+          child: ElevatedButton.icon(
+            icon: Icon(
+              Icons.ac_unit,
+              color: Colors.red,
+            ),
+            onPressed: () {},
+            label: Text('Hello'),
+          ),
+        ),
       ),
     ));
   }
