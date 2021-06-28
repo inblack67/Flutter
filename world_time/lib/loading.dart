51f225c79345a93e6d18038794f62e0d86fc8d54
ryuzaki <ryuzaki>
2021-06-28 12:43:02 +0530

    get time

--- world_time/lib/loading.dart
+++ world_time/lib/loading.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,6 +9,22 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getTime() async {
+    Response res = await get(
+        Uri.parse('http://worldtimeapi.org/api/timezone/Europe/london'));
+    Map data = jsonDecode(res.body);
+    String utcDate = data['utc_datetime'];
+    String utcOffset = data['utc_offset'];
+    DateTime currentTime = DateTime.parse(utcDate)
+        .add(Duration(hours: int.parse(utcOffset.substring(1, 3))));
+    print(currentTime);
+  }
+
+  @override
+  void initState() {
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Text('Loading...'));
--- world_time/lib/main.dart
+++ world_time/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/home',
+      initialRoute: '/',
       routes: {
         '/': (context) => Loading(),
         '/home': (context) => Home(),
