1035e647d8506a3a99f0f2579a8a9c28860e8eda
ryuzaki <ryuzaki>
2021-07-11 11:01:16 +0530

    refinements

--- graphql/bin/service.dart
+++
@@ -1,7 +0,0 @@
-import 'package:graphql/client.dart';
-
-void main(List<String> args) {
-  final countriesReq = GfetchCountriesReq();
-  final client = initClient('https://countries.trevorblades.com');
-  client.request(countriesReq).listen((res) {});
-}
